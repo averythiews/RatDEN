af1b7ff1b65576e63ba1e443ea1610bb180747bd
JacobDeming <JacobDeming91@gmail.com>
2025-09-03 15:54:40 -0700

    Updating the coffee shop website with RAT

--- ratDINApp/CLI.js
+++ ratDINApp/CLI.js
@@ -16,33 +16,33 @@ theMap=[
                                 content:"<p>Now searching for files regarding the entities of the Northern Cascades</p><p id='error'>HIDDEN FILES DETECTED</p>",
                                 commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities: ',
                                 upstream:[
-                                    {pageName:"arborealEntity01",
-                                        content:"<br><p>The Forest of the North Cascades is an ancient and enigmatic force of nature that embodies decay, rot, and the relentless march of natural entropy. It is considered an elder entity, wielding a mysterious and formidable power that remains largely unknown to those who study or confront it. This entity appears to have the extraordinary ability to generate new life forms from the organic detritus that accumulates within its domain, turning said matter into creatures that then serve its inscrutable purposes. These emergent beings are often unpredictable and dangerous, capable of venturing beyond the Forest's immediate boundaries to inflict chaos upon nearby human settlements. Their origins from the decayed landscape make them inherently hostile and difficult to control, posing a significant threat to civilian populations and infrastructure. The creatures' ability to escape the Forest's influence underscores the need for vigilant monitoring and strategic containment.</p>",
-                                        commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01: ',
+                                    {pageName:"theRootedOne",
+                                        content:"<br><p>The Rooted One of the North Cascades is an ancient and enigmatic force of nature that embodies decay, rot, and the relentless march of natural entropy. It is considered an elder entity, wielding a mysterious and formidable power that remains largely unknown to those who study or confront it. This entity appears to have the extraordinary ability to generate new life forms from the organic detritus that accumulates within its domain, turning said matter into creatures that then serve its inscrutable purposes. These emergent beings are often unpredictable and dangerous, capable of venturing beyond The Rooted One's immediate boundaries to inflict chaos upon nearby human settlements. Their origins from the decayed landscape make them inherently hostile and difficult to control, posing a significant threat to civilian populations and infrastructure. The creatures' ability to escape The Rooted One's influence underscores the need for vigilant monitoring and strategic containment.</p>",
+                                        commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne: ',
                                         upstream:[
                                             {pageName:"containment",
-                                                content:"<br><p>These emergent beings are often unpredictable and dangerous, capable of venturing beyond the Forest's immediate boundaries to inflict chaos upon nearby human settlements. Their origins from the decayed landscape make them inherently hostile and difficult to control, posing a significant threat to civilian populations and infrastructure. The creatures' ability to escape the Forest's influence underscores the need for vigilant monitoring and strategic containment.</p>",
-                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01/containment: ',
+                                                content:"<br><p>These emergent beings are often unpredictable and dangerous, capable of venturing beyond The Rooted One's immediate boundaries to inflict chaos upon nearby human settlements. Their origins from the decayed landscape make them inherently hostile and difficult to control, posing a significant threat to civilian populations and infrastructure. The creatures' ability to escape The Rooted One's influence underscores the need for vigilant monitoring and strategic containment.</p>",
+                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne/containment: ',
                                                 upstream:[]
                                             },
                                             {pageName:"communication",
-                                                content:"<br><p>The Forest possesses the ability to communicate psychically, often through disturbing and horrifying means. Those who attempt to engage with it mentally risk developing symptoms of wood rot, which gradually distort their physical form into grotesque, inhospitable shapes. This process is particularly severe in humans, although some supernatural entities with a shared affinity for wild magic exhibit greater resistance, likely due to their inherent magical resilience.</p>",
-                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01/communication: ',
+                                                content:"<br><p>The Rooted One possesses the ability to communicate psychically, often through disturbing and horrifying means. Those who attempt to engage with it mentally risk developing symptoms of wood rot, which gradually distort their physical form into grotesque, inhospitable shapes. This process is particularly severe in humans, although some supernatural entities with a shared affinity for wild magic exhibit greater resistance, likely due to their inherent magical resilience.</p>",
+                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne/communication: ',
                                                 upstream:[]
                                             },
                                             {pageName:"scope",
-                                                content:"<br><p>The influence of the Forest extends across the entire Northern Cascades, making it a significant concern for regional security. To mitigate its impact, it is recommended to maintain a steady influx of drifters—individuals who can serve as sacrificial offerings—to distract the Forest and prevent it from expanding further into critical areas. This strategy aims to contain the threat while minimizing risk to essential personnel.</p>",
-                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01/scope: ',
+                                                content:"<br><p>The influence of The Rooted One extends across the entire Northern Cascades, making it a significant concern for regional security. To mitigate its impact, it is recommended to maintain a steady influx of drifters—individuals who can serve as sacrificial offerings—to distract The Rooted One and prevent it from expanding further into critical areas. This strategy aims to contain the threat while minimizing risk to essential personnel.</p>",
+                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne/scope: ',
                                                 upstream:[]
                                             },
                                             {pageName:"realityManipulation",
-                                                content:"<br><p>Beyond its biological and psychic capabilities, the Forest can manipulate reality within its domain. It creates vast, seemingly endless caverns, towering mountains, and dense, labyrinthine trails that challenge any who ventures into its territory. These altered landscapes are perilous and escape often requires the use of extrasensory devices or advanced magical tools. However, prolonged exposure to the Forest's domain only amplifies its brutality and horror, making escape increasingly difficult and dangerous when longer expeditions are undertaken.</p>",
-                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01/realityManipulation: ',
+                                                content:"<br><p>Beyond its biological and psychic capabilities, The Rooted One can manipulate reality within its domain. It creates vast, seemingly endless caverns, towering mountains, and dense, labyrinthine trails that challenge any who ventures into its territory. These altered landscapes are perilous and escape often requires the use of extrasensory devices or advanced magical tools. However, prolonged exposure to The Rooted One's domain only amplifies its brutality and horror, making escape increasingly difficult and dangerous when longer expeditions are undertaken.</p>",
+                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne/realityManipulation: ',
                                                 upstream:[]
                                             },
                                             {pageName:"conductors",
-                                                content:"<br><p>Propositions to involve local Wild Magic conductors have been evaluated but ultimately rejected. The primary concerns are that these conductors may pose additional threats or lack the necessary expertise, rendering them ineffective or even counterproductive. Nonetheless, this approach remains open for reconsideration should circumstances change or new information emerge about their potential utility in managing the Forest's influence.</p>",
-                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/arborealEntity01/conductors: ',
+                                                content:"<br><p>Propositions to involve local Wild Magic conductors have been evaluated but ultimately rejected. The primary concerns are that these conductors may pose additional threats or lack the necessary expertise, rendering them ineffective or even counterproductive. Nonetheless, this approach remains open for reconsideration should circumstances change or new information emerge about their potential utility in managing The Rooted One's influence.</p>",
+                                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/theRootedOne/conductors: ',
                                                 upstream:[]
                                             }
                                         ],
@@ -50,22 +50,38 @@ theMap=[
                                 ],
                                 hidden:[
                                     {pageName:"liliaSilverstone",
-                                        content:"<p>Age: 25</p><p>Species: Therianthrope(Silver Wolf)</p><p>Address: 3400 N Clark Street</p><p>First confirmed user of IOI and confirmed Wild Mangic conductor.</p><p>Silver-haired lycan therianthrope with increased strength and regenerative abilities. Appears to be the leader of the Wild Magic cult/coven operating within the area designated as the Northern Cascades. Blood has been collected and is being used in testing to determine whether its properties can be used to augment human/canine operatives.</p>",
+                                        content:"<p>Age: 25</p><p>Species: Therianthrope(Silver Wolf)</p><p>Address: 3400 N Clark Street</p><p>First confirmed user of NC-IOI-01 and confirmed Wild Mangic conductor.</p><p>Silver-haired lycan therianthrope with increased strength and regenerative abilities. Appears to be the leader of the Wild Magic cult/coven operating within the area designated as the Northern Cascades. According to records published by the therianthropic group, she is in a close relationship with Stephanie Richardson. Then again, these individuals appear to bond with one-another like animals and, as such, any records of true romantic relations should be regarded with skepticism.</><p>Blood has been collected from this entity and is being used in testing to determine whether its properties can be used to augment human/canine operatives.</p>",
                                         commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/liliaSilverstone: ',
                                         upstream:[]
                                     },
                                     {pageName:"stephanieRichardson",
-                                        content:"<p>Age: N/A</p><p>Species: Therianthrope(River Otter)</p><p>Address:185 Rinehart Way, Second Floor</p><p>Second confirmed user of IOI and confirmed Wild Mangic conductor.</p><p>Owner of the Roc Cafe and Coffee on the main street of Marble Basin. According to records she is 38 years old but visual documentation of the woman has shown her to be far younger. It is theorized that the Wild Magic release may have affected her physical age. An epigentic clock would provide definitive results but a complete autopsy would help to explain how certain bones/ligiments react to the process of de-aging.</p>",
+                                        content:"<p>Age: N/A</p><p>Species: Therianthrope(River Otter)</p><p>Address:123 Granite Road, Second Floor</p><p>Second confirmed user of NC-IOI-01 and confirmed Wild Magic conductor.</p><p>Owner of the Roc Cafe and Coffee on the main street of Marble Basin. According to records she is 38 years old but visual documentation of the woman has shown her to be far younger. It is theorized that the Wild Magic release may have affected her physical age. An epigentic clock would provide definitive results but a complete autopsy would help to explain how certain bones/ligiments react to the process of de-aging.</p><p>Brother by the name of Jake Richardson was declared missing in 2021</p>",
                                         commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/stephanieRichardson: ',
                                         upstream:[]
-
+                                    },
+                                    {pageName:"Seraphina Bellweather",
+                                        content:"<p>Age: 25</p><p>Species: Therianthrope(Northern Spotted Owl)</p><p>Address: 19 Rinnfeld Road</p><p>Owner of the local occult shop Wytches and confirmed Wild Magic conductor.</p><p>Family history of occultism has provided the individual with a greater knowledge of the unknown than others within the town. She has only started to study Wild Magic in earnest after her infection with therianthropy and is not to be considered much of a threat if forced into engagement. Is the primary author of the 'Fictional' supernatural romance series called Crimson Crescent Fang so as to pass information to individuals outside of Marble Basin. AI censroship tools have been put in place to prevent anything potentially harmful from leaking. In terms of artistic value, Crimson Crescent Fang is mediocre at best and it is doubtful the work will have any lasting impact.</p>",
+                                        commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/seraphinaBellweather: ',
+                                        upstream:[]
+                                    },
+                                    {pageName:"Greg 'The Rat' Kent",
+                                        content:"<p>Age: 20</p><p>Species: RatRatRatRatRat</p><p>Address: He's In The Goddamn Walls</p><p>Fuck you guys, I am writing my own entry!</p><p>The Rat is easily one of the best hackers that is not currently owned by some bullshit corporation or government entity. The Rat works for whoever he wants, whenever he wants, and wherever her wants. Now that he is in the system, he's gonna crawl through your walls and take all your shit. Fuck with him or any of his buddies and a whole lot of radioactive documents are going to find themselves in very capable hands. In other words, I am the giant rat that makes all of the rules. Now let's see what kind of trouble we can get ourselves into.",
+                                        commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/entities/gregKent: ',
+                                        upstream:[]
+                                        
                                     }
                                 ]
                             },
-                            {pageName:"iois",
+                            {pageName:"ioi",
                                 content:"<p>Now searching for Items of Interest within the Northern Cascades</p>",
-                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/arborealEntity01/scope: ',
-                                upstream:[]
+                                commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/ioi: ',
+                                upstream:[
+                                    {pageName:"nc-IOI-01",
+                                        content:"<p>NC-IOI-01 (Wilderness Fang) is a 4-inch long Machairodus fang with mossy cracks running across its length/width. The IOI provides its wielder with the ability to open up Wild Magic breaches with little effort. All one needs to do in order to activate the IOI is stab the length through a body part in one continuous motion. Upon extraction from the wound, the hole present within the user's body will become a breach through which Wild Magic will flow. The rapid transference of this Wild Magic through the human body tends to cause cases of therianthropy within the individual and thus allows them to channel Wild Magic even after the hole/breach has been healed</p><p>This item is not currently within the posession of the GBI and could pose a substantial risk should certain groups get their hands on it.</p><p>GBI scientists wish to examine the IOI and test its usage on non-human subjects whilst also studying what would happen should an individual be pierced with the IOI multiple times.</p>",
+                                        commandLine:'[Root@57.3.55.8][GPI_Base]northCascades/ioi/nc-IOI-01: ',
+                                        upstream:[]
+                                    }
+                                ]
                             }
                         ]
                     }
@@ -76,7 +92,7 @@ theMap=[
                 commandLine:"[Root@Localhost~]Within_The_Rat_Den/gregThoughts: ",
                 upstream:[
                     {pageName:'theGPI',
-                        content:"<br><p>Who the Hell are these guys? They look like park rangers but Markus has no idea who any of them are. They won't even communicate with the guy and their security is INSANE. We are talking AES with a 256bit key levels of encryption. I'll break this shit wide open though. Rats can get into anything.</p>",
+                        content:"<br><p>Who the Hell are these guys? They look like park rangers but Markus has no idea who any of them are. They won't even communicate with the guy and their security is INSANE. We are talking AES with a 256bit key levels of encryption. I'll break this shit wide open though. Rats can get into anything.</p><p>Finally! Paydirt! We are into the GPI and have access to a large portion of their files that cover the immediate Marble Basin area. Some files, like those created the rest of the pack and me, are hidden but can be entered so long as you understand the way in which these people label stuff. To test yourself out, try searching for my file.</p><p>Hint: My name is Greg Kent</p>",
                         commandLine:"[Root@Localhost~]Within_The_Rat_Den/gregThoughts/theGPI: ",
                         upstream:[]
                     }
